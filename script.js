989a29c16e48d415028202540dd3e97f299dfca2
MIKHAIL GAVRILOV <mishwit@inbox.ru>
2022-11-09 01:43:38 +0300

    starting

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ menu.addEventListener("click", (event) => {
     sidebar.style.display = "none";
   }
 });
+
+const xx = (a, b) => {
+  return a + b;
+};
+
+const res1 = xx(1, 2); // 3
+const res2 = xx(100, 23); // 123
+
+console.log(res1, res2);
+
+const name1 = (name, surname) => {
+  return `Имя: ${name} Фамилия: ${surname}`;
+};
+
+name1("Михаил", "Гаврилов");
